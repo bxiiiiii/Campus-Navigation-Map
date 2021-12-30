176d44d0822f9a4380eb0f76f5d3cfbfa5264689
bxiiiiii <1551097460@qq.com>
2021-12-30 23:30:13 +0800

    update UI

--- src/UI.c
+++ src/UI.c
@@ -4,18 +4,24 @@ void UI(AdjList *list)
 {
     int choice;
     while(1) {
+	system("clear");
         printf("[1]管理员\n");
         printf("[2]用户\n");
         printf("请选择登录身份:");
         scanf("%d", &choice);
         if(choice == 1) {
-            admini_UI(list);
+            if(!Login(list))
+                admini_UI(list);
+            else {
+                printf("密码错误,请重新输入\n");
+                continue;
+            }
             break;
         } else if(choice = 2) {
             user_UI(list);
             break;
         } else
-            printf("输入有误，请重新输入\n");
+            printf("输入有误,请重新输入\n");
     }
 }
  
@@ -23,17 +29,19 @@ void admini_UI(AdjList *list)
 {
     int choice;
     while(1) {
-        printf("[1]insert node\n");
-        printf("[2]modify node\n");
-        printf("[3]delete node\n");
-        printf("[4]insert path\n");
-        printf("[5]modify path\n");
-        printf("[6]delete path\n");
-        printf("[7]save change\n");
-        printf("[8]exit\n");
-        printf("//[9]show node\n");
-        printf("//[10]show path\n");
-        printf("请输入选择:");
+       system("clear");
+        printf("\n\n\n\n\t\t\t[1]新建地点\n");
+        printf("\t\t\t[2]修改地点\n");
+        printf("\t\t\t[3]删除地点\n");
+        printf("\t\t\t[4]新建路径\n");
+        printf("\t\t\t[5]修改路径\n");
+        printf("\t\t\t[6]删除路径\n");
+        printf("\t\t\t[7]修改密码\n");
+        printf("\t\t\t[8]保存修改\n");
+        printf("\t\t\t[9]退出\n");
+        printf("\t\t\t[10]查看地点\n");
+        printf("\t\t\t[11]查看地图\n");
+        printf("\t      请输入选择:");
         scanf("%d", &choice);
         switch (choice) {
             case 1:
@@ -55,33 +63,42 @@ void admini_UI(AdjList *list)
                 DeleteEdge(list);
                 break;
             case 7:
-                SaveGraph(list);
+                ChangePass(list);
                 break;
             case 8:
-                return ;
+                SaveGraph(list);
+                break;
             case 9:
+                return ;
+            case 10:
                 PrintNode(list);
                 break;
-            case 10:
+            case 11:
                 PrintPath(list);
                 break;
             default:
                 printf("输入有误，请重新输入\n");
         }
-    }
+    
+        fflush(stdin);
+        //getchar();
+        char tem[30];
+        printf("请输入任意键继续:");
+        scanf("%s", tem);}
 }
  
 void user_UI(AdjList *list)
 {
     int choice;
     while(1) {
-        printf("[1]查看地图\n");
-        printf("[2]查看景点\n");
-        printf("[3]最优路径查询\n");
-        printf("[4]find1");
-        printf("[5]find2\n");
-        printf("[6]return");
-        printf("请输入选择:");
+        system("clear");
+        printf("\n\n\n\n\n\t\t\t[1]查看地图\n");
+        printf("\t\t\t[2]查看景点\n");
+        printf("\t\t\t[3]查询最短路径\n");
+        printf("\t\t\t[4]查询优美路径\n");
+        printf("\t\t\t[5]查询绿荫路径\n");
+        printf("\t\t\t[6]退出\n\n");
+        printf("\t      请输入选择:");
         scanf("%d", &choice);
         switch (choice) {
             case 1:
@@ -103,5 +120,10 @@ void user_UI(AdjList *list)
             default:
                 printf("输入有误，请重新输入\n");
         }
+        fflush(stdin);
+        //getchar();
+        char tem[30];
+        printf("输入任意内容继续:");
+        scanf("%s", tem);
     }
 }
